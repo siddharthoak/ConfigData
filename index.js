9e327b34c18e6bec37c8d244dbcd667e601ef4e8
Siddharth Oak <siddharo@in.ibm.com>
2020-02-28 06:32:03 +0530

    Added content

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+var msg = process.env.APP_MSG;
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end(msg);
+
+});
+
+fs.readFile('/opt/app-root/secure/myapp.sec', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+});
+
+
+var port = 8081;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
